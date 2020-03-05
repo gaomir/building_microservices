6115342b00593ad9fc041375cfb79636e53224ea
Marius Armenteras <marmenteras@multiestetica.com>
2020-03-05 08:22:41 +0100

    First route.

--- Episode_1/main.go
+++ Episode_1/main.go
@@ -1,8 +1,14 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
+	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+		log.Println("Hello World!")
+	})
 
 	http.ListenAndServe(":9090", nil)
 
