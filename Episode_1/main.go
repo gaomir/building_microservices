97089578534913403c6b8061163d5507d2fee33f
Marius Armenteras <marmenteras@multiestetica.com>
2020-03-09 09:46:23 +0100

    Read from request body.

--- Episode_1/main.go
+++ Episode_1/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		log.Println("Hello World!")
+		d, _ := ioutil.ReadAll(r.Body)
+
+		log.Printf("Data %s\n", d)
 	})
 
 	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
